396216303dd145a866519b0ce51f65b3713f3305
Ijalana Afolabi <iijalana@gmail.com>
2023-11-17 22:12:40 +0100

    adding the new node to the stack

---
+++ 0-push_node.c
@@ -0,0 +1,28 @@
+#include "header.h"
+#include <stdlib.o>
+#include <stdio.h>
+/*
+ * pushnode - push to the stack and make it point to the head
+ * @head: head of the stack
+ * @n: data to be pushed
+ * Return: nothing
+ */
+void pushnode(stack_t **head, int n)
+{
+	stack_t *temp = *head;
+	stack_t *newNode = malloc(sizeof(stack_t));
+	
+	if (newNode == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (temp != NULL)
+	{
+		temp->prev = newNode;
+	}
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
+	*head = newNode;
+}
